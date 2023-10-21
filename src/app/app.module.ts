44e7b42065a7edfcc4ba086a9745553fffc9e3e3
ali.pooladmast <ali.pooladmast.gh@gmail.com>
2023-10-21 17:59:08 +0200

    add coin list and detail routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { CoinListComponent } from './coin-list/coin-list.component';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CoinListComponent, CoinDetailComponent],
   imports: [
     BrowserModule,
     FormsModule,
