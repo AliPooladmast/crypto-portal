b27b7d69fe4bb0428db6a88c95334bc8202e9df2
ali.pooladmast <ali.pooladmast.gh@gmail.com>
2023-10-22 19:27:31 +0200

    add api call for coin detail page

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coin-detail',
   templateUrl: './coin-detail.component.html',
-  styleUrls: ['./coin-detail.component.scss']
+  styleUrls: ['./coin-detail.component.scss'],
 })
-export class CoinDetailComponent {
+export class CoinDetailComponent implements OnInit {
+  coinData: any;
+  coinId!: string | null;
+  days: number = 1;
+  currency: string = 'USD';
 
+  constructor(private api: ApiService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.coinId = params.get('id');
+    });
+
+    this.getCoinData();
+  }
+
+  getCoinData() {
+    if (!this.coinId) return;
+    this.api.getCurrencyById(this.coinId).subscribe((res) => {
+      this.coinData = res;
+    });
+  }
 }
