2e4cd27ce7461b2089dd9e1cf3ccc0e8b97eee6e
ali.pooladmast <ali.pooladmast.gh@gmail.com>
2023-10-22 14:02:02 +0200

    call apis in list component

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
   styleUrls: ['./coin-list.component.scss'],
 })
-export class CoinListComponent {}
+export class CoinListComponent implements OnInit {
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getBannerData();
+    this.getAllData();
+  }
+
+  getBannerData() {
+    this.api.getTrendingCurrency('USD').subscribe((res) => console.log(res));
+  }
+
+  getAllData() {
+    this.api.getCurrency('USD').subscribe((res) => console.log(res));
+  }
+}
