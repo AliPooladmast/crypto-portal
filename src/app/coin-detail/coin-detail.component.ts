249d596509efc4bc8d9f29a687cb2b253b023e99
ali.pooladmast <ali.pooladmast.gh@gmail.com>
2023-10-22 23:22:54 +0200

    add ng chart and basic configs

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { ActivatedRoute } from '@angular/router';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-coin-detail',
@@ -12,6 +14,37 @@ export class CoinDetailComponent implements OnInit {
   coinId!: string | null;
   days: number = 1;
   currency: string = 'USD';
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+      },
+    ],
+    labels: [],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1,
+      },
+    },
+
+    plugins: {
+      legend: { display: true },
+    },
+  };
+
+  public lineChartType: ChartType = 'line';
+  @ViewChild(BaseChartDirective) myLineChart!: BaseChartDirective;
 
   constructor(private api: ApiService, private route: ActivatedRoute) {}
 
